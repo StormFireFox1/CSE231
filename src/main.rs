0d9115121440931db6f0897a53b43436c4e01978
Matei-Alexandru Gardus <matei@gard.us>
2023-05-09 18:57:15 -0700

    Zero out bottom 4 bits to "16-byte align" stack pointer
    It's a hack, but if it works, it works. Erroring out won't do much
    for our current code anyway, so who cares?

--- src/main.rs
+++ src/main.rs
@@ -133,15 +133,19 @@ global our_code_starts_here
 {}
 not_bool_err:
   mov rdi, 1
+  and rsp, -16
   call snek_error
 not_num_err:
   mov rdi, 2
+  and rsp, -16
   call snek_error
 invalid_arg_err:
   mov rdi, 3
+  and rsp, -16
   call snek_error
 overflow_err:
   mov rdi, 4
+  and rsp, -16
   call snek_error
 ",
         result
